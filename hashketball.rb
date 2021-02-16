30a8d74b4d29743134f939443a97b0ad5391f3c0
Andre Hochi <andrehochiwork@gmail.com>
2021-02-16 03:43:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,15 +6,15 @@ def game_hash
       :team_name => "Brooklyn Nets"
       :colors => ["Black", "White"]
       :players => [{
-        :Player_name => "",
+        :Player_name => "Alan Anderson",
         :Number => 0,
-        :Shoe => 0,
-        :Points => 0,
-        :rebounds => 0,
-        :Assists => 0,
-        :Steals => 0,
-        :Blocks => 0,
-        :Slam_Dunks => 0,
+        :Shoe => 16,
+        :Points => 22,
+        :rebounds => 12,
+        :Assists => 12,
+        :Steals => 3,
+        :Blocks => 1,
+        :Slam_Dunks => 1,
       },
       {
         :Player_name => "",
