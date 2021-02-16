0c3d442cf3bf395f0b4d23d4fbf9a8eb46c738f9
Andre Hochi <andrehochiwork@gmail.com>
2021-02-16 03:41:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,36 @@
 # Write your code here!
+
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "Brooklyn Nets"
+      :colors => ["Black", "White"]
+      :players => [{
+        :Player_name => "",
+        :Number => 0,
+        :Shoe => 0,
+        :Points => 0,
+        :rebounds => 0,
+        :Assists => 0,
+        :Steals => 0,
+        :Blocks => 0,
+        :Slam_Dunks => 0,
+      }]
+    }
+    :away => {
+      :team_name => "Charlotte Hornets"
+      :colors => []
+      :players => [{
+        :Player_name => "",
+        :Number => 0,
+        :Shoe => 0,
+        :Points => 0,
+        :rebounds => 0,
+        :Assists => 0,
+        :Steals => 0,
+        :Blocks => 0,
+        :Slam_Dunks => 0,
+      }]
+    }
+  }
+end
