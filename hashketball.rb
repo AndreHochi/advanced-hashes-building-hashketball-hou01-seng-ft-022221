2b85e88ae6368f5ea3972923cf2b3b3c93510acb
Andre Hochi <andrehochiwork@gmail.com>
2021-02-16 03:47:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,7 @@ def game_hash
     }
     :away => {
       :team_name => "Charlotte Hornets"
-      :colors => []
+      :colors => ["Turquoise", "Purple"]
       :players => [{
         :Player_name => "",
         :Number => 0,
